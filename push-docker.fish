1bd412555d54348d5cdb50913ccf7b1a6b0ecbae
shinyay <shinya.com@gmail.com>
2019-03-25 15:26:05 +0900

    Modify logic for calling push method from fish function

--- push-docker.fish
+++ push-docker.fish
@@ -1,9 +1,19 @@
 #!/usr/bin/env fish
 
-if not set -q argv[1]
-  echo 'push-docker.fish <DOCKERHUB_USER_NAME> <DOCKERHUB_USER_PASSWORD>'
-else if not set -q argv[2]
-  echo 'push-docker.fish DOCKERHUB_USER_NAME <DOCKERHUB_USER_PASSWORD>'
+function push_docker
+  if not set -q $argv[1]
+    echo 'push-docker.fish <DOCKERHUB_USER_NAME> <DOCKERHUB_USER_PASSWORD>'
+  else if not set -q $argv[2]
+    echo 'push-docker.fish DOCKERHUB_USER_NAME <DOCKERHUB_USER_PASSWORD>'
+  else
+    ./mvnw compile jib:build -Djib.to.auth.username=$argv[1] -Djib.to.auth.password=$argv[2]
+  end
+end
+
+set arg_count (count $argv)
+if test $arg_count -ne 2
+  echo 'Error for Argument'
+  echo 'USAGE: push-docker.fish <DOCKERHUB_USER_NAME> <DOCKERHUB_USER_PASSWORD>'
 else
-  ./mvnw compile jib:build -Djib.to.auth.username=$argv[1] -Djib.to.auth.password=$argv[2]
+  push_docker
 end
